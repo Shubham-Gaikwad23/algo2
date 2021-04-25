bed30d0f10ec095a8acbcdd499f71dc0571a6767
Shubham Gaikwad <Shubham.gaikwad23@gmail.com>
2021-04-25 18:13:46 +0530

    Do it in 1 pass only: O(n) time & O(n) space

--- Problem solving/LinkedList/remove_nth_node.c
+++ Problem solving/LinkedList/remove_nth_node.c
@@ -8,14 +8,13 @@
  * };
  */
 
-struct ListNode* reverse(struct ListNode *);
-int count(struct ListNode*);
-
 struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
     struct ListNode* cur;
+    struct ListNode* stopper;
     struct ListNode* tmp;
     int i, total_nodes;
     
+    // Manually handle case for list of size less than or equal to 1
     if(head==NULL)
         return head;
     else if(head->next == NULL) {
@@ -24,49 +23,26 @@ struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
         return head;
     }
     
-    total_nodes = count(head);
-    n = total_nodes - n + 1;
+    // Set stopper 'n' nodes ahead of head of the list
+    for(i=0, stopper=head; i<n; ++i, stopper = stopper->next);
     
-    for(i=0, cur=head; i<n-2; cur=cur->next, ++i);
-    if(n-2 == -1) {
-        tmp = head->next;
-        free(head);
-        head = tmp;   
+    // If stopper became 'NULL' then we need to delete first node
+    if(stopper==NULL) {
+        tmp = head;
+        head = head->next;
+        free(tmp);   
     }
     else {
+        // Set 'cur' node to head
+        // Keep moving 'stopper' and 'cur' node one node ahead of themselves
+        // Until end of list is reached
+        for(cur=head; stopper->next!=NULL; cur=cur->next, stopper=stopper->next);
+
+        // Now 'cur' is just behind the node to be deleted and 'stopper' is at last node of the list
         tmp = cur->next;
         cur->next = cur->next->next;
         free(tmp);
     }
-        
+    
     return head;    
-}
-
-struct ListNode* reverse(struct ListNode *head) {
-    struct ListNode* left;
-    struct ListNode* cur;
-    struct ListNode* right;
-        
-    if(head == NULL || head->next == NULL)
-        return head;
-    
-    for(left=NULL, cur=head, right=cur->next; cur!=NULL; ) {
-        cur->next = left;
-        
-        left=cur;
-        cur=right;
-        if(right)
-            right=right->next;
-    }
-        
-    
-    return left;
-}
-
-int count(struct ListNode* head) {
-    int i;
-    struct ListNode* cur;
-    for(i=0, cur=head; cur!=NULL; i++, cur=cur->next);
-    
-    return i;
 }
